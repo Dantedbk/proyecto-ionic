9d25f942d94747da1753c1797c243f0ddc3a4ce8
Dantedbk <101567058+Dantedbk@users.noreply.github.com>
2022-09-14 01:12:45 -0300

    s

--- src/app/recover/recover.page.ts
+++ src/app/recover/recover.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recover',
@@ -7,8 +8,10 @@ import { Router } from '@angular/router'
   styleUrls: ['./recover.page.scss'],
 })
 export class RecoverPage implements OnInit {
-
-  constructor(private router : Router) { }
+  correo : String
+  constructor(
+    private router : Router,
+    private toastController: ToastController) { }
 
   ngOnInit() {
   }
@@ -16,6 +19,36 @@ export class RecoverPage implements OnInit {
 
   volverHome() {
     this.router.navigate(["./home"])
+  }
+
+  async recuperar(){
+    let regex = new RegExp(/^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/)
+    if (this.correo == null || "" ){
+      const toast = await this.toastController.create({
+        message : "Introduzca un correo electronico",
+        duration: 2000 
+    })
+    toast.present();
+    }
+    else if (!regex.test(this.correo.toString())){
+      const toast = await this.toastController.create({
+        message : "Introduzca un formato de correo válido",
+        duration: 2000
+
+    })
+    toast.present();
+    }
+    else if (this.correo != null || ""){
+      this.correo = null
+      const toast = await this.toastController.create({
+        message : "De existir registro, se enviarán instrucciones de restablecimiento",
+        duration: 2000
+
+    })
+    toast.present();
+    }
+    
+
   }
 
 }
